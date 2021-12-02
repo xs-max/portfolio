4e528b54896696eeec20e6bf08af1d90620eb1d2
code_max <ogbujigodwinmax@gmail.com>
2021-12-03 00:17:27 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,37 @@
+const header = document.querySelector('.header');
+const phone = document.querySelector('.phone');
+const menu = document.querySelector('.menu__action')
+const about = document.querySelector('#about');
+const portfolio = document.querySelector('#portfolio');
+const contact = document.querySelector('#contact');
+
+
+
+const showMobileNav = () => {
+    phone.classList.remove('phone__nav');
+    phone.classList.add('show__nav');
+    menu.innerHTML = `<i class="far fa-window-close"></i>`;
+
+}
+
+const hideMobileNav = () => {
+    phone.classList.remove('show__nav');
+    phone.classList.add('phone__nav');
+    menu.innerHTML = `<i class="fas fa-bars"></i>`;
+}
+
+header.addEventListener('click', (e) => {
+    
+    if(e.target.classList[1] == 'fa-bars') {
+        showMobileNav();
+    }
+    if(e.target.classList[1] == 'fa-window-close') {
+        hideMobileNav();
+    }
+})
+
+phone.addEventListener('click', (e) => {
+    if(e.target.parentElement.className == 'nav__item') {
+        hideMobileNav();
+    }
+})
